18bb329ff574dd55a18cdc148f9be7c8803921af
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-04-05 09:32:53 +0000

    [Finish]4-6/4-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action:configure_permtted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  
+  
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+  
+  protected
+  
+  def configure_permtted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end
 end
