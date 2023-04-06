f546b670295ae0df081c1e561d3ad2c70e2ef2a5
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-04-06 05:20:48 +0000

    [Finish]4-11/4-12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action:configure_permtted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
-    about_path
+    post_images_path
   end
   
   
