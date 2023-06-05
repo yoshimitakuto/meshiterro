69b3e22e4daad2eacd10a17cc0863bbe50fcddbe
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-06-05 07:38:22 +0000

    [Add]seed file

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -7,7 +7,7 @@ class PostImage < ApplicationRecord
  has_many :favorites, dependent: :destroy
  
  validates :shop_name, presence: true
- validates :image, presence: 
+ validates :image, presence: true
  
  def get_image
   unless image.attached?
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,12 +2,12 @@ const { environment } = require('@rails/webpacker')
 
 module.exports = environment
 
-const webpack = require("webpack")
+const webpack = require('webpack')
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
-    $:'jquery/src/jquery',
-    jQuery:'jquery/src/jquery',
-    Popper:'popper.js'
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
   })
 )
